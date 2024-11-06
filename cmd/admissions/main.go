08bb466d8eefe274afd344f20a9030bf5d3ce6e9
GregoryKogan <grisha.koganovskiy@gmail.com>
2024-11-06 19:09:17 +0300

    feat: add user management and database initialization

--- cmd/admissions/main.go
+++ cmd/admissions/main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
 
+	"github.com/L2SH-Dev/admissions/internal/auth"
 	"github.com/L2SH-Dev/admissions/internal/config"
+	"github.com/L2SH-Dev/admissions/internal/database"
 	"github.com/L2SH-Dev/admissions/internal/logging"
+	"github.com/L2SH-Dev/admissions/internal/passwords"
 	"github.com/L2SH-Dev/admissions/internal/ping"
+	"github.com/L2SH-Dev/admissions/internal/users"
 	"github.com/L2SH-Dev/admissions/internal/validation"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/viper"
+	"gorm.io/gorm"
 )
 
 func main() {
 	config.Init()
 	logging.Init()
+
+	db := initDB()
 
 	e := echo.New()
 
@@ -26,11 +34,32 @@ func main() {
 	serveFrontend(e)
 
 	pingHandler := ping.NewPingHandler()
+	usersHandler := users.NewUsersHandler(
+		users.NewUsersService(
+			users.NewUsersRepo(db),
+		),
+		auth.NewAuthService(
+			passwords.NewPasswordsService(
+				passwords.NewPasswordsRepo(db),
+			),
+		),
+	)
 
 	api := e.Group("/api")
 	pingHandler.AddRoutes(api)
+	usersHandler.AddRoutes(api)
 
 	startServer(e)
+}
+
+func initDB() *gorm.DB {
+	db, err := database.Connect()
+	if err != nil {
+		slog.Error("Failed to connect to the database", slog.Any("error", err))
+		panic(err)
+	}
+
+	return db
 }
 
 func addMiddleware(e *echo.Echo) {
