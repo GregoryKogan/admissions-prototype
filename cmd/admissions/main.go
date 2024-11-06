3c7321b92097a2f4883f4a26f43e7c1bedb9e733
GregoryKogan <grisha.koganovskiy@gmail.com>
2024-11-06 15:22:17 +0300

    refactor: simplify main function by extracting frontend serving and server starting logic

--- cmd/admissions/main.go
+++ cmd/admissions/main.go
@@ -3,59 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/L2SH-Dev/admissions/internal/auth"
 	"github.com/L2SH-Dev/admissions/internal/config"
-	"github.com/L2SH-Dev/admissions/internal/database"
 	"github.com/L2SH-Dev/admissions/internal/logging"
 	"github.com/L2SH-Dev/admissions/internal/ping"
-	"github.com/L2SH-Dev/admissions/internal/users"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/viper"
-	"golang.org/x/exp/slog"
 )
 
 func main() {
 	config.Init()
 	logging.Init()
 
-	initDatabase()
-
 	e := echo.New()
 
 	addMiddleware(e)
 
-	e.Static("/", "ui/dist")
-	e.File("/", "ui/dist/index.html")
+	serveFrontend(e)
+
+	pingHandler := ping.NewPingHandler()
 
 	api := e.Group("/api")
-	ping.AddRoutes(api)
-	auth.AddRoutes(api)
+	pingHandler.AddRoutes(api)
 
-	port := viper.GetString("server.port")
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
-}
-
-func initDatabase() {
-	db, err := database.Connect()
-	if err != nil {
-		slog.Error("Failed to connect to the database", slog.Any("error", err))
-		os.Exit(1)
-	}
-
-	err = users.Migrate(db)
-	if err != nil {
-		slog.Error("Failed to migrate the database", slog.Any("error", err))
-		os.Exit(1)
-	}
-
-	err = users.CreateDefaultRoles(db)
-	if err != nil {
-		slog.Error("Failed to create default roles", slog.Any("error", err))
-		os.Exit(1)
-	}
+	startServer(e)
 }
 
 func addMiddleware(e *echo.Echo) {
@@ -69,4 +41,14 @@ func addMiddleware(e *echo.Echo) {
 		AllowOrigins: []string{fmt.Sprintf("http://localhost:%s", port)},
 		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
 	}))
+}
+
+func serveFrontend(e *echo.Echo) {
+	e.Static("/", "ui/dist")
+	e.File("/", "ui/dist/index.html")
+}
+
+func startServer(e *echo.Echo) {
+	port := viper.GetString("server.port")
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
 }
