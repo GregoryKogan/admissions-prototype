8f66e461b0addbceeb36760f1516bec8529a99b1
Gregory Koganovskiy <grisha.koganovskiy@gmail.com>
2024-11-09 20:20:50 +0300

    feat: add mock secret management for testing purposes

--- internal/secrets/secrets.go
+++ internal/secrets/secrets.go
@@ -1,11 +1,28 @@
 package secrets
 
-import "os"
+import (
+	"os"
+)
+
+// Mock secrets for testing
+var mockSecrets = make(map[string]string)
 
 func ReadSecret(secret string) (string, error) {
+	if value, ok := mockSecrets[secret]; ok {
+		return value, nil
+	}
+
 	buffer, err := os.ReadFile("/run/secrets/" + secret)
 	if err != nil {
 		return "", err
 	}
 	return string(buffer), nil
+}
+
+func SetMockSecret(secret, value string) {
+	mockSecrets[secret] = value
+}
+
+func ClearMockSecrets() {
+	mockSecrets = make(map[string]string)
 }
