7c72c445df95dadf949f49dc8c119bf9f6ab6b4e
Gregory Koganovskiy <grisha.koganovskiy@gmail.com>
2024-11-13 02:00:04 +0300

    test: add unit tests for AuthService methods; include Register, Login, Refresh, and Logout scenarios

--- internal/users/auth/service_test.go
+++ internal/users/auth/service_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/L2SH-Dev/admissions/internal/users/auth"
+	"github.com/L2SH-Dev/admissions/internal/users/auth/authjwt"
 	"github.com/L2SH-Dev/admissions/internal/users/auth/passwords"
 	"github.com/stretchr/testify/assert"
 )
@@ -23,4 +24,77 @@ func setupTestService(t *testing.T) auth.AuthService {
 	})
 
 	return auth.NewAuthService(authRepo, passwordsService)
+}
+
+func TestAuthService_Register(t *testing.T) {
+	service := setupTestService(t)
+
+	err := service.Register(1, "Password$123")
+	assert.NoError(t, err)
+}
+
+func TestAuthService_Login(t *testing.T) {
+	service := setupTestService(t)
+
+	err := service.Register(1, "Password$123")
+	assert.NoError(t, err)
+
+	tokenPair, err := service.Login(1, "Password$123")
+	assert.NoError(t, err)
+	assert.NotNil(t, tokenPair)
+	assert.NotEmpty(t, tokenPair.Access)
+	assert.NotEmpty(t, tokenPair.Refresh)
+}
+
+func TestAuthService_Login_InvalidPassword(t *testing.T) {
+	service := setupTestService(t)
+
+	err := service.Register(1, "Password$123")
+	assert.NoError(t, err)
+
+	tokenPair, err := service.Login(1, "WrongPassword")
+	assert.Error(t, err)
+	assert.Nil(t, tokenPair)
+}
+
+func TestAuthService_Refresh(t *testing.T) {
+	service := setupTestService(t)
+
+	err := service.Register(1, "Password$123")
+	assert.NoError(t, err)
+
+	tokenPair, err := service.Login(1, "Password$123")
+	assert.NoError(t, err)
+	assert.NotNil(t, tokenPair)
+
+	newTokenPair, err := service.Refresh(tokenPair.Refresh)
+	assert.NoError(t, err)
+	assert.NotNil(t, newTokenPair)
+	assert.NotEmpty(t, newTokenPair.Access)
+	assert.NotEmpty(t, newTokenPair.Refresh)
+}
+
+func TestAuthService_Refresh_InvalidToken(t *testing.T) {
+	service := setupTestService(t)
+
+	newTokenPair, err := service.Refresh("invalid token")
+	assert.Error(t, err)
+	assert.Nil(t, newTokenPair)
+}
+
+func TestAuthService_Logout(t *testing.T) {
+	service := setupTestService(t)
+
+	err := service.Register(1, "Password$123")
+	assert.NoError(t, err)
+
+	tokenPair, err := service.Login(1, "Password$123")
+	assert.NoError(t, err)
+	assert.NotNil(t, tokenPair)
+
+	service.Logout(1)
+
+	claims, err := service.IsTokenCached(&authjwt.JWTClaims{UserID: 1, Type: "access"})
+	assert.NoError(t, err)
+	assert.False(t, claims)
 }
