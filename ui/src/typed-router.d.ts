d5ce9f630ddf521c130e8989ef8372350fa6a767
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2024-12-06 23:13:55 +0300

    feature: add 404 error page and update router for dynamic route handling

--- ui/src/typed-router.d.ts
+++ ui/src/typed-router.d.ts
@@ -19,6 +19,7 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/[...404]': RouteRecordInfo<'/[...404]', '/:404(.*)', { 404: ParamValue<true> }, { 404: ParamValue<false> }>,
     '/admin/dashboard': RouteRecordInfo<'/admin/dashboard', '/admin/dashboard', Record<never, never>, Record<never, never>>,
     '/admin/registrations': RouteRecordInfo<'/admin/registrations', '/admin/registrations', Record<never, never>, Record<never, never>>,
     '/login': RouteRecordInfo<'/login', '/login', Record<never, never>, Record<never, never>>,
