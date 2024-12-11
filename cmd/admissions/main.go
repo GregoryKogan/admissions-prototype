781d715e3009ca61ac229a4e417416ae1248aabf
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2024-12-12 01:16:57 +0300

    feature: add exams handler to admissions main application

--- cmd/admissions/main.go
+++ cmd/admissions/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/L2SH-Dev/admissions/internal/admin"
 	"github.com/L2SH-Dev/admissions/internal/config"
 	"github.com/L2SH-Dev/admissions/internal/datastore"
+	"github.com/L2SH-Dev/admissions/internal/exams"
 	"github.com/L2SH-Dev/admissions/internal/logging"
 	"github.com/L2SH-Dev/admissions/internal/ping"
 	"github.com/L2SH-Dev/admissions/internal/regdata"
@@ -24,6 +25,7 @@ func main() {
 		ping.NewPingHandler,
 		users.NewUsersHandler,
 		regdata.NewRegistrationDataHandler,
+		exams.NewExamsHandler,
 	)
 
 	admin.CreateDefaultAdmin(storage)
