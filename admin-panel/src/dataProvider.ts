ecb92bf88350735412a808d0e7308172616518d5
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2024-12-16 05:15:24 +0300

    feature: initialize admin panel with React, Vite, and TypeScript setup

---
+++ admin-panel/src/dataProvider.ts
@@ -0,0 +1,14 @@
+import { fetchUtils } from "react-admin";
+import {
+  IDataProviderConfig,
+  defaultPrimaryKeys,
+  defaultSchema,
+} from "@raphiniert/ra-data-postgrest";
+
+export const config: IDataProviderConfig = {
+  apiUrl: "http://localhost:3333",
+  httpClient: fetchUtils.fetchJson,
+  defaultListOp: "eq",
+  primaryKeys: defaultPrimaryKeys,
+  schema: defaultSchema,
+};
