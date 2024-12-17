19347582a6465b8052842ac220949663e3a1a595
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2024-12-17 23:35:27 +0300

    refactor: remove unused AuthLoader component from global components declaration

--- ui/src/components.d.ts
+++ ui/src/components.d.ts
@@ -7,7 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    AuthLoader: typeof import('./components/AuthLoader.vue')['default']
     CreateExam: typeof import('./components/CreateExam.vue')['default']
     Exam: typeof import('./components/Exam.vue')['default']
     ExamCard: typeof import('./components/ExamCard.vue')['default']
